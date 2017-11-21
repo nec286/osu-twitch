061978b2ad41c42996aa087fb928ba8e60561f0a
tetsuo286 <peter.t.ludlow@gmail.com>
2017-11-21 14:03:22 +1100

    Add some FF hacks

--- src/config/config.js
+++ src/config/config.js
@@ -3,13 +3,16 @@ export default function() {
   case 'production':
     return {
       ebs: {
-        url: ''
+        // url: 'https://osu-twitch-ebs.atami.io'
+        projectKey: 'AIzaSyDIa9LWRsExVFkD4YTg_Zqij-rKfzx7q8Q'
       }
     };
   default:
     return {
       ebs: {
-        url: 'https://10.1.1.183:81'
+        url: 'https://10.1.1.183:81',
+        projectKey: ''
+        // url: 'https://osu-twitch-ebs.atami.io'
       }
     };
   }
--- src/context/index.js
+++ src/context/index.js
@@ -3,11 +3,12 @@ import config from 'config/config';
 import Root from 'stores/Root';
 
 export default (state) => {
-  const { ebs } = config();
+  const { ebs, projectKey } = config();
 
   const request = axios.create({
     baseURL: `${ ebs.url }`,
-    timeout: 10000
+    timeout: 10000,
+    headers: { 'x-api-key': projectKey }
   });
 
   return {
