848a6d085f7ebb04eb79bffa6dde9e936416de7a
tetsuo286 <peter.t.ludlow@gmail.com>
2017-11-29 10:35:18 +1100

    Fix issue with rank 0

--- src/components/viewer/profile/Showcase.js
+++ src/components/viewer/profile/Showcase.js
@@ -3,14 +3,22 @@ import Component from 'inferno-component';
 import { Level } from './';
 import { Badge, Flag, Grade } from 'components/viewer';
 
+class Rank extends Component {
+  render({ label, value }) {
+    return (
+      <Badge className="rank" label={ label } value={ value > 0 ? value : '-' } />
+    );
+  }
+}
+
 export default class extends Component {
   render({ profile }) {
     const ranks = ['ss', 's', 'a'];
     return (
       profile &&
       <div className="showcase d-flex">
-        <Badge className="rank" label={  <i className="icon-globe" /> } value={ profile.pp_rank } />
-        <Badge className="rank" label={  <Flag country={ profile.country } /> } value={ profile.pp_country_rank } />
+        <Rank label={  <i className="icon-globe" /> } value={ profile.pp_rank } />
+        <Rank label={  <Flag country={ profile.country } /> } value={ profile.pp_country_rank } />
         { ranks.map(rank => <Grade label={ rank.toUpperCase() } value={ profile[`count_rank_${rank}`] } /> ) }
         <Level value={ profile.level } />
       </div>
