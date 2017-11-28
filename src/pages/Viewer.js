1dc794955c4de83813c06014e772670be6300e3b
tetsuo286 <peter.t.ludlow@gmail.com>
2017-11-28 16:29:09 +1100

    Tweak font and css

--- src/pages/Viewer.js
+++ src/pages/Viewer.js
@@ -1,7 +1,7 @@
 import Inferno from 'inferno';
 import Component from 'inferno-component';
 import { connect } from 'inferno-mobx';
-import { Banner, Tabs, Error } from 'components/viewer';
+import { Tabs, Error } from 'components/viewer';
 
 @connect(['state', 'store'])
 export default class extends Component {
@@ -11,10 +11,9 @@ export default class extends Component {
   }
 
   render({ state, children }) {
-    const { lastError, user } = state;
+    const { lastError } = state;
     return (
       <main className="container-fluid p-0">
-        <Banner user={ user } />
         <Tabs />
         <div className="page">
           { !lastError ? children : <Error error={ lastError } /> }
