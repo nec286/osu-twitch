38df533ef5e3486225fd301169ee32c8b5261e01
tetsuo286 <peter.t.ludlow@gmail.com>
2017-12-04 12:54:59 +1100

    Format numbers - country/global rank

--- src/components/viewer/Showcase.js
+++ src/components/viewer/Showcase.js
@@ -1,5 +1,6 @@
 import Inferno from 'inferno';
 import Component from 'inferno-component';
+import { formatNo } from 'utils';
 import { Badge, Flag, Grade } from 'components/viewer';
 
 export default class extends Component {
@@ -8,8 +9,8 @@ export default class extends Component {
     return (
       profile &&
       <div className="showcase d-flex">
-        <Badge label={  <i className="icon-globe" /> } value={ profile.pp_rank > 0 ? profile.pp_rank : '-' } />
-        <Badge label={  <Flag country={ profile.country } /> } value={ profile.pp_country_rank > 0 ? profile.pp_country_rank : '-'  } />
+        <Badge label={  <i className="icon-globe" /> } value={ profile.pp_rank > 0 ? formatNo(profile.pp_rank) : '-' } />
+        <Badge label={  <Flag country={ profile.country } /> } value={ profile.pp_country_rank > 0 ? formatNo(profile.pp_country_rank) : '-'  } />
         { ranks.map(rank => <Grade label={ rank.toUpperCase() } value={ profile[`count_rank_${rank}`] } /> ) }
       </div>
     );
