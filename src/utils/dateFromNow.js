4cd66f569a7442a4de8275ef69a51ee0db5996ac
tetsuo286 <peter.t.ludlow@gmail.com>
2017-12-05 10:00:51 +0100

    Address timezone woes

--- src/utils/dateFromNow.js
+++ src/utils/dateFromNow.js
@@ -1,5 +1,6 @@
 import distanceInWordsToNow from 'date-fns/distance_in_words_to_now';
+import parse from 'date-fns/parse';
 
 export default function(date, options) {
-  return distanceInWordsToNow(new Date(date + ' UTC+8'), options);
+  return distanceInWordsToNow(parse(date + '+08:00'), options);
 }
